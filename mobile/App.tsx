54ed3a10979a094405b9f91348f691c521a45928
Jeziel Marques <jezielmarques7@gmail.com>
2020-08-11 02:45:53 -0300

    landing page created

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins';
+
+import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+}
---
+++ mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    justifyContent: 'center',
+    backgroundColor: '#8257E5',
+  },
+  banner: {
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    marginTop: 80,
+    lineHeight: 30,
+    fontFamily: 'Poppins_400Regular',
+  },
+  titleBold: {
+    fontFamily: 'Poppins_600SemiBold',
+  },
+  buttonsContainer: {
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    padding: 24,
+    height: 150,
+    width: '48%',
+    borderRadius: 8,
+    backgroundColor: '#666',
+    justifyContent: 'space-between',
+  },
+  buttonPrimary: {
+    backgroundColor: '#9871f5',
+  },
+  buttonSecondary: {
+    backgroundColor: '#04d361',
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Archivo_700Bold',
+  },
+  totalConnections: {
+    fontSize: 12,
+    marginTop: 40,
+    maxWidth: 140,
+    lineHeight: 20,
+    color: '#d4c2ff',
+    fontFamily: 'Poppins_400Regular',
+  },
+});
+
+export default styles;
