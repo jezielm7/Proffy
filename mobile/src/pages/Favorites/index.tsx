fd10acbfda9acd842c79c8b601dae2d32d591fb1
Jeziel Marques <jezielmarques7@gmail.com>
2020-08-11 07:49:57 -0300

    created header component

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
   return (
     <View style={styles.container}>
-      
+      <PageHeader title="Meus proffys favoritos"/>
     </View>
   );
 }
